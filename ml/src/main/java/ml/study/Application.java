6c9ce220fa96cf1d3c9b318fe9018ed94f809b21
hemingliang <916812579@qq.com>
2018-05-11 19:05:57 +0800

    修改main方法

--- ml/src/main/java/ml/study/Application.java
+++ ml/src/main/java/ml/study/Application.java
@@ -1,8 +1,17 @@
 package ml.study;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author hemingliang-1291
  * @date 2018/5/11 16:50
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
